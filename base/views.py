8b63d2e133415f24f2b7314ef55d0dd9e2c55458
koomekevo <koome.kevo@gurucrafts.net>
2022-06-23 12:02:19 +0300

    Modify createRoom() function

--- base/views.py
+++ base/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .models import Room
-
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Frontend developers'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developers'},
+# ]
 
 def home(request):
     rooms = Room.objects.all()
@@ -21,6 +21,6 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
-
-    context = {}
+    form = RoomForm()
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
